f33c8651634bdd455740de16c7840e8e9ded4948
r33 <44626926+r33-code@users.noreply.github.com>
2020-06-20 01:23:10 -0300

    Desafio: Conceitos de ReactJS finalizado

--- src/App.js
+++ src/App.js
@@ -1,30 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('repositories', {
+      title: `Repositório: ${Date.now()}`,
+      url: "github.com/r33-code/desafio-conceitos-reactjs",
+      techs: [
+        "Node.js",
+        "Javascript",
+        "Express"
+      ]
+    })
+
+    const repository = response.data
+
+    setRepositories([...repositories, repository])
+
   }
 
   async function handleRemoveRepository(id) {
+
     // TODO
+    await api.delete(`repositories/${id}`).then(() => {
+      const repository = repositories.find(repository => repository.id === id)
+      const newRepositories = [ ...repositories ]
+      newRepositories.splice(repository, 1)
+  
+      setRepositories(newRepositories)
+    })
+
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    <>
+      <div>
+        <ul data-testid="repository-list">
+          {repositories.map(repository => (
+            <li key={repository.id}>
+              {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+              <button onClick={() => handleRemoveRepository(`${repository.id}`)}>
+                Remover
+            </button>
+            </li>
+          ))}
+        </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
+    </>
   );
 }
 
